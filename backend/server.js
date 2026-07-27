be594aaeed4a063cca9293080945d56e29ac22b5
drixo-dev <dhyeypatel835@gmail.com>
2026-07-27 20:01:31 +0530

    security: configure CORS to use environment variables for frontend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const volunteerRoutes = require('./routes/volunteer');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
